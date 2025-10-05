342b0237ac817fbd64a02ad7076ef3b24e9d1a80
wanto-production <ikwansatria3974@gmail.com>
2025-10-05 13:53:30 +0700

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin({ preset: 'vercel' }),
+    nitroV2Plugin(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
